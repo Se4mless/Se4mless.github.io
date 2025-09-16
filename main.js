0db66f9073366bed68202db71f95af90dcac7a45
Se4mless <joshuabeddows1@gmail.com>
2025-09-16 12:42:37 +0000

    started js

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 const alpha = {}
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
-function decrypt(){
+// Load Alpha Dict
+for (let i = 0; i < alphabet.length; i++) {
 
+    const element = alphabet[i];
+    alpha[element] = i;
+    
+}
+
+function getOffsetChars(startChar,offset) {
+    let idx = alpha[startChar];
+
+    return alphabet[(idx + offset) % alphabet.length]
+}
+
+function decrypt(){
+    // Split the text into sections of len expected
+    let expected = int(document.querySelector("input").value);
+    let text = String(document.querySelector("#cypher").value)
+    text = text.replace(/\s+/g, '')
+    let split = text.match(/.{1,4}/g) || []
 }
